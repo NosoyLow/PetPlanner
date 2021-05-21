0acdb042c992e91fd5ed644d31ab89ee55f8fcea
edgardovdz <edgardo_vdz@hotmail.com>
2021-05-20 22:55:20 -0500

    agenda
    news

--- src/app/pages/agenda/agenda-view/agenda-view.module.ts
+++ src/app/pages/agenda/agenda-view/agenda-view.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-
+import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
 import { IonicModule } from '@ionic/angular';
-
+import { CalendarModule, DateAdapter } from 'angular-calendar';
+import { NgbModalModule } from '@ng-bootstrap/ng-bootstrap';
 import { AgendaViewPageRoutingModule } from './agenda-view-routing.module';
-
 import { AgendaViewPage } from './agenda-view.page';
 
 
@@ -15,6 +15,10 @@ import { AgendaViewPage } from './agenda-view.page';
     FormsModule,
     IonicModule,
     AgendaViewPageRoutingModule,
+    CalendarModule.forRoot({
+      provide: DateAdapter,
+      useFactory: adapterFactory,
+    }),
   ],
   declarations: [AgendaViewPage]
 })
--- src/app/pages/agenda/agenda-view/agenda-view.page.ts
+++ src/app/pages/agenda/agenda-view/agenda-view.page.ts
@@ -1,6 +1,46 @@
-import { Component, Input, NgModule, OnInit, Output } from '@angular/core';
+import { Component, Input, NgModule, OnInit, Output, TemplateRef, ViewChild } from '@angular/core';
 import { MenuController, PopoverController } from '@ionic/angular';
 import { AgendaEditPage } from '../agenda-edit/agenda-edit.page';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import {
+  CalendarEvent,
+  CalendarEventAction,
+  CalendarEventTimesChangedEvent,
+  CalendarView,
+} from 'angular-calendar';
+
+import {
+  startOfDay,
+  endOfDay,
+  subDays,
+  addDays,
+  endOfMonth,
+  isSameDay,
+  isSameMonth,
+  addHours,
+} from 'date-fns';
+
+// colores
+
+const colors: any = { // inecesary
+  red: {
+    primary: '#ad2121',
+    secondary: '#FAE3E3',
+  },
+  blue: {
+    primary: '#1e90ff',
+    secondary: '#D1E8FF',
+  },
+  yellow: {
+    primary: '#e3bc08',
+    secondary: '#FDF1BA',
+  },
+};
+
+interface MyEvent extends CalendarEvent { // Customizando para agregar otra variable tipo string a los eventos.
+  description: string;
+  repeat: boolean;
+} 
 
 @Component({
   selector: 'app-agenda-view',
@@ -10,21 +50,129 @@ import { AgendaEditPage } from '../agenda-edit/agenda-edit.page';
 
 export class AgendaViewPage implements OnInit {
 
-  titulos:string;
+  @ViewChild('modalContent', { static: true }) modalContent:
+  TemplateRef<any>;
 
-  constructor(private menu: MenuController) { }
+  
+  //@ViewChild(MenuController) menu: MenuController;
+  
 
 
+  CalendarView = CalendarView;
+  titulos:string; 
+  
+  viewDate: Date = new Date();
+
+  modalData: {
+    action: string;
+    event: MyEvent;
+  }
+
+// acciones en calendar
+/*
+actions: CalendarEventAction[] = [
+  {
+    label: '<i class="fas fa-fw fa-pencil-alt"></i>',
+    a11yLabel: 'Edit',
+    onClick: ({ event }: { event: MyEvent }): void => {
+      this.handleEvent('Edited', event);
+    },
+  },
+  {
+    label: '<i class="fas fa-fw fa-trash-alt"></i>',
+    a11yLabel: 'Delete',
+    onClick: ({ event }: { event: MyEvent }): void => {
+      this.events = this.events.filter((iEvent) => iEvent !== event);
+      this.handleEvent('Deleted', event);
+    },
+  },
+];
+*/
+  //constructor(private menu: MenuController) { }
+
+ constructor(private modal: NgbModal) {} //pendiente
 
   ngOnInit() {
     console.log(this.titulos);
   }
 
- 
-
-  openFirst() {
-    this.menu.enable(true, 'first');
-    this.menu.open('first');
+  handleEvent(action: string, event: MyEvent): void {
+    this.modalData = { event, action };
+    this.modal.open(this.modalContent, {size: 'lg'});
   }
+  
+// eventos
+
+events: MyEvent[] = [
+  
+  {
+    start: subDays(startOfDay(new Date()), 1),
+    end: addDays(new Date(), 1),
+    title: 'A 3 day event',
+    description: 'Agenda de mi primer perro',
+    repeat: true,
+    color: colors.red,
+    //actions: this.actions,
+    allDay: true,
+    resizable: {
+      beforeStart: true,
+      afterEnd: true,
+    },
+    draggable: true,
+  },
+  /*
+  {
+    start: startOfDay(new Date()),
+    title: 'An event with no end date',
+    color: colors.yellow,
+    actions: this.actions,
+  },
+  {
+    start: subDays(endOfMonth(new Date()), 3),
+    end: addDays(endOfMonth(new Date()), 3),
+    title: 'A long event that spans 2 months',
+    color: colors.blue,
+    allDay: true,
+  },
+  {
+    start: addHours(startOfDay(new Date()), 2),
+    end: addHours(new Date(), 2),
+    title: 'A draggable and resizable event',
+    color: colors.yellow,
+    actions: this.actions,
+    resizable: {
+      beforeStart: true,
+      afterEnd: true,
+    },
+    draggable: true,
+  },
+  */
+];
+
+// fin eventos
+
+ addEvent(): void { // realizar
+   this.events = [
+     ...this.events,
+     {
+       title: 'Nuevo evento',
+       description: 'Nueva descripcion',
+       repeat: true,
+       start: startOfDay(new Date()),
+       end: endOfDay(new Date()),
+       color: colors.red,
+     },
+   ];
+ }
+
+   deleteEvent(eventToDelete: MyEvent) {
+ this.events = this.events.filter((event) => event !== eventToDelete);
+}
+
+
+ // openFirst() {
+   // this.menu.enable(true, 'first');
+  //  this.menu.open('first');
+//  }
 
 }
